6bf26e762086e1332b095880c49e2fc74c9e4c02
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2022-10-16 18:59:03 -0300

    16/10/2022

---
+++ src/components/pages/UserBlog.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import UserBio from "../molecules/UserBio";
+import PostListWrapper from "../molecules/PostListWrapper";
+
+import Default from "../templates/Default";
+
+export default function UserBlog() {
+  const { userId } = useParams();
+
+  const [posts, setPosts] = React.useState([]);
+  const [user, setUser] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(`https://62c4e487abea8c085a7e022a.mockapi.io/users/${userId}/posts`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setUser(data[0].userData);
+      });
+  }, [userId]);
+
+  return (
+    <Default>
+      <div className="user-blog">
+        <UserBio user={user} />
+        <PostListWrapper posts={posts} />
+      </div>
+    </Default>
+  );
+}
